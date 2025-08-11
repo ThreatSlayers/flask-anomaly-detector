2836eee30344fefcaf7bc5d226052f015f7e8cb9
Sreypich1234 <6023010015@camtech.edu.kh>
2025-08-11 10:47:31 +0700

    app.py

--- app.py
+++ app.py
@@ -61,4 +61,8 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT dynamically
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+
